529b80c77176910ab6ef157a50525b2d85665ab3
Jimmy Bergman <jimmy@atomia.com>
2017-02-28 15:02:47 +0100

    Fixed SendInvoices version

--- SendInvoices.py
+++ SendInvoices.py
@@ -1,17 +1,15 @@
 ###
 ### Script name: Send invoices
 ### Description: Sends a batch of invoices specified by id.
-### Version: 1.0
+### Version: 1.1
 ### Author: Jimmy Bergman
 ###
 ### Parameters:
 ### invoice_ids - Array of invoice ID:s to send
-###    example: [System.Guid("5332eba0-b55d-44a4-a94a-a960a4f25d04"), System.Guid("0a019a00-562a-4ae4-acff-6a1607021255")]
+###    example: [ "5332eba0-f6aa-4b57-a340-9c141f9d252f", "0a019a00-6c62-49f7-a251-d04829beaff8" ]
 
 import System
 
-invoice_ids = [System.Guid("5332eba0-b55d-44a4-a94a-a960a4f25d04"), System.Guid("0a019a00-562a-4ae4-acff-6a1607021255")]
-
-billingApi.SendInvoices(System.Array[System.Guid](invoice_ids), None)
+billingApi.SendInvoices(System.Array[System.Guid](map(System.Guid,invoice_ids)), None)
 
 print str(len(invoice_ids)) + " invoices sent"
